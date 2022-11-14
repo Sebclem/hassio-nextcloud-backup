9271437ec6ccb9392e154a5d0430fd11245387fc
SebClem <seb6596@gmail.com>
2022-11-14 16:12:05 +0100

    :hammer: Extract data from cloud file name

--- nextcloud_backup/backend/src/services/backupConfigService.ts
+++ nextcloud_backup/backend/src/services/backupConfigService.ts
@@ -1,27 +1,25 @@
 import fs from "fs";
-import Joi from "joi"
+import Joi from "joi";
 import logger from "../config/winston.js";
-import type { BackupConfig } from "../types/services/backupConfig.js"
+import type { BackupConfig } from "../types/services/backupConfig.js";
 import backupConfigValidation from "../types/services/backupConfigValidation.js";
-
 
 const backupConfigPath = "/data/backupConfigV2.json";
 
-
-export function validateBackupConfig(config: BackupConfig){
+export function validateBackupConfig(config: BackupConfig) {
   const validator = Joi.object(backupConfigValidation);
   return validator.validateAsync(config, {
-    abortEarly: false
+    abortEarly: false,
   });
 }
 
-export function saveBackupConfig(config: BackupConfig){
+export function saveBackupConfig(config: BackupConfig) {
   fs.writeFileSync(backupConfigPath, JSON.stringify(config, undefined, 2));
 }
 
 export function getBackupConfig(): BackupConfig {
   if (!fs.existsSync(backupConfigPath)) {
-    logger.warn("Config file not found, creating default one !")
+    logger.warn("Config file not found, creating default one !");
     const defaultConfig = getBackupDefaultConfig();
     saveBackupConfig(defaultConfig);
     return defaultConfig;
@@ -39,7 +37,7 @@ export function getBackupDefaultConfig(): BackupConfig {
         enabled: false,
       },
       webdav: {
-        enabled: false
+        enabled: false,
       },
     },
     exclude: {
@@ -49,6 +47,18 @@ export function getBackupDefaultConfig(): BackupConfig {
     autoStopAddon: [],
     password: {
       enabled: false,
-    }
-  }
+    },
+  };
+}
+
+export function templateToRegexp(template: string) {
+  let regexp = template.replace("{date}", "(?<date>\\d{4}-\\d{2}-\\d{2})");
+  regexp = regexp.replace("{hour}", "(?<hour>\\d{4})");
+  regexp = regexp.replace("{hour_12}", "(?<hour12>\\d{4}(AM|PM))");
+  regexp = regexp.replace("{type}", "(?<type>Auto|Manual|)")
+  regexp = regexp.replace("{type_low}", "(?<type>auto|manual|)")
+  return regexp.replace(
+    "{ha_version}",
+    "(?<version>\\d+\\.\\d+\\.\\d+(b\\d+)?)"
+  );
 }
--- nextcloud_backup/frontend/src/types/webdav.ts
+++ nextcloud_backup/frontend/src/types/webdav.ts
@@ -1,11 +1,11 @@
-import type { DateTime } from "luxon";
-
 export interface WebdavBackup {
   id: string;
   name: string;
   size: number;
   lastEdit: string;
   path: string;
+  haVersion?: string;
+  creationDate?: string;
 }
 
 export interface WebdavDeletePayload {
