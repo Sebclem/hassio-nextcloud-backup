9a02bfc6b8d0e063fe184f6d9ba09d4e995b1fcd
SebClem <seb6596@gmail.com>
2023-01-13 18:14:30 +0100

    :hammer: Add autoclean setting menu

---
+++ nextcloud_backup/frontend/src/components/settings/BackupConfig/BackupConfigAutoClean.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-card variant="elevated" elevation="7">
+    <v-card-title class="bg-light-blue-darken-4 text-center">
+      Auto Clean
+    </v-card-title>
+    <v-card-text>
+      <v-row class="mt-0" v-if="!loading">
+        <v-col class="" cols="12" md="6">
+          <v-row dense>
+            <v-col>
+              <v-switch
+                label="Auto clean Home Assistant Backups"
+                v-model="data.autoClean.homeAssistant.enabled"
+                hide-details="auto"
+                density="compact"
+                inset
+                :loading="loading"
+                color="orange"
+              ></v-switch>
+            </v-col>
+          </v-row>
+          <v-fade-transition>
+            <v-row dense v-if="data.autoClean.homeAssistant.enabled">
+              <v-col>
+                <div class="text-subtitle-1 text-medium-emphasis">
+                  Number of backup to keep
+                </div>
+                <v-text-field
+                  v-model="data.autoClean.homeAssistant.nbrToKeep"
+                  type="number"
+                  hide-details="auto"
+                  density="compact"
+                  variant="outlined"
+                  color="orange"
+                  min="1"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-fade-transition>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-row dense>
+            <v-col>
+              <v-switch
+                label="Auto clean Cloud Backups"
+                v-model="data.autoClean.webdav.enabled"
+                hide-details="auto"
+                density="compact"
+                inset
+                :loading="loading"
+                color="orange"
+              ></v-switch>
+            </v-col>
+          </v-row>
+          <v-fade-transition>
+            <v-row dense v-if="data.autoClean.webdav.enabled">
+              <v-col>
+                <div class="text-subtitle-1 text-medium-emphasis">
+                  Number of backup to keep
+                </div>
+                <v-text-field
+                  v-model="data.autoClean.webdav.nbrToKeep"
+                  type="number"
+                  hide-details="auto"
+                  density="compact"
+                  variant="outlined"
+                  color="orange"
+                  min="1"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-fade-transition>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+<script setup lang="ts">
+import { useBackupConfigStore } from "@/stores/backupConfig";
+import { storeToRefs } from "pinia";
+
+defineProps<{ loading: boolean }>();
+const backupConfigStore = useBackupConfigStore();
+const { data } = storeToRefs(backupConfigStore);
+</script>
